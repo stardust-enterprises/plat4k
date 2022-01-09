2bc12e12f038a19de8d275b4da2467509cf8c3f4
lambdagg <lambda@jikt.im>
2022-01-09 19:40:54 +0100

    gradle: automagically updated kotlin to 1.6.10 and made the whole process easier
    also, reformatted some things in the buildscript following the @stardustenterprises/yanl changes.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,7 @@
-@file:Suppress("UNUSED_VARIABLE")
-
-import org.jetbrains.dokka.gradle.DokkaTask
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     `java-library`
-    kotlin("jvm") version "1.6.0"
-    id("org.jetbrains.dokka") version "1.6.0"
+    kotlin("jvm")
+    id("org.jetbrains.dokka")
     `maven-publish`
     signing
     id("io.github.gradle-nexus.publish-plugin") version "1.0.0"
@@ -20,15 +15,11 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.0")
+    implementation(kotlin("stdlib"))
 }
 
 tasks {
-    withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "1.8"
-    }
-
-    val dokkaHtml by getting(DokkaTask::class) {
+    dokkaHtml {
         dokkaSourceSets {
             configureEach {
                 skipDeprecated.set(true)
@@ -40,78 +31,87 @@ tasks {
             }
         }
     }
+
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+    /* Artifacts */
+
+    // Source artifact, including everything the 'main' does but not compiled.
+    val sourcesJar by registering(Jar::class) {
+        archiveClassifier.set("sources")
+        from(sourceSets.main.get().allSource)
+    }
+
+    val javadocJar by registering(Jar::class) {
+        archiveClassifier.set("javadoc")
+        dependsOn(dokkaHtml)
+        from(dokkaHtml)
+    }
 }
 
-val sourcesJar by tasks.registering(Jar::class) {
-    archiveClassifier.set("sources")
-    from(sourceSets.main.get().allSource)
-}
-
-val javadocJar by tasks.registering(Jar::class) {
-    archiveClassifier.set("javadoc")
-    dependsOn(tasks.dokkaHtml)
-    from(tasks.dokkaHtml)
-}
+val artifactTasks = arrayOf(
+    tasks["sourcesJar"],
+    tasks["javadocJar"]
+)
 
 artifacts {
-    archives(sourcesJar)
-    archives(javadocJar)
+    artifactTasks.forEach(::archives)
 }
 
 val projectName = project.name
 val desc = "Platform identifier library for the JVM."
-val devs = arrayOf("xtrm", "lambdagg")
+val authors = arrayOf("xtrm", "lambdagg")
 val repo = "stardust-enterprises/$projectName"
 
-publishing {
-    publications {
-        val main by creating(MavenPublication::class) {
-            from(components["java"])
-            artifact(sourcesJar.get())
-            artifact(javadocJar.get())
+publishing.publications {
+    create<MavenPublication>("mavenJava") {
+        from(components["java"])
+        artifactTasks.forEach(::artifact)
 
-            pom {
-                name.set(projectName)
-                description.set(desc)
-                url.set("https://github.com/$repo")
+        pom {
+            name.set(projectName)
+            description.set(desc)
+            url.set("https://github.com/$repo")
 
-                licenses {
-                    license {
-                        name.set("ISC License")
-                        url.set("https://opensource.org/licenses/ISC")
-                        distribution.set("repo")
-                    }
-                }
-                developers {
-                    devs.forEach {
-                        developer {
-                            id.set(it)
-                            name.set(it)
-                        }
-                    }
-                }
-                scm {
-                    connection.set("scm:git:git://github.com/$repo.git")
-                    developerConnection.set("scm:git:ssh://github.com/$repo.git")
-                    url.set("https://github.com/$repo")
+            licenses {
+                license {
+                    name.set("ISC License")
+                    url.set("https://opensource.org/licenses/ISC")
+                    distribution.set("repo")
                 }
             }
+
+            developers {
+                authors.forEach {
+                    developer {
+                        id.set(it)
+                        name.set(it)
+                    }
+                }
+            }
+
+            scm {
+                connection.set("scm:git:git://github.com/$repo.git")
+                developerConnection.set("scm:git:ssh://github.com/$repo.git")
+                url.set("https://github.com/$repo")
+            }
         }
+
+        // Configure the signing extension to sign this Maven artifact.
+        signing.sign(this)
     }
 }
 
-nexusPublishing {
-    repositories {
-        sonatype {
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+// Set up the Sonatype artifact publishing.
+nexusPublishing.repositories.sonatype {
+    nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+    snapshotRepositoryUrl.set(
+        uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+    )
 
-            username.set(project.properties["NEXUS_USERNAME"] as? String ?: return@sonatype)
-            password.set(project.properties["NEXUS_PASSWORD"] as? String ?: return@sonatype)
-        }
-    }
-}
-
-signing {
-    sign(publishing.publications)
+    // Skip this step if environment variables NEXUS_USERNAME or NEXUS_PASSWORD aren't set.
+    username.set(properties["NEXUS_USERNAME"] as? String ?: return@sonatype)
+    password.set(properties["NEXUS_PASSWORD"] as? String ?: return@sonatype)
 }
