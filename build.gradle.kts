6521ad234997f523e53802523ec97e5e28da769d
lambdagg <lambda@jikt.im>
2022-01-10 20:51:29 +0100

    gradle: updated dokka behavior added repo desc in the outputted index added sources in the outputted files added external sources for jna

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-@file:Suppress("UNUSED_VARIABLE")
+import java.net.URL
 
 plugins {
     `java-library`
@@ -23,10 +23,43 @@ dependencies {
 
 tasks {
     dokkaHtml {
-        dokkaSourceSets {
-            configureEach {
-                skipDeprecated.set(true)
-                reportUndocumented.set(true)
+        val moduleFile = File(projectDir, "MODULE.temp.MD")
+
+        run {
+            // In order to have a description on the rendered docs, we have to have
+            // a file with the # Module thingy in it. That's what we're
+            // automagically creating here.
+
+            doFirst {
+                moduleFile.writeText("# Module $projectName\n$desc")
+            }
+
+            doLast {
+                moduleFile.delete()
+            }
+        }
+
+        moduleName.set(projectName)
+
+        dokkaSourceSets.configureEach {
+            displayName.set("$projectName github")
+            includes.from(moduleFile.path)
+
+            skipDeprecated.set(false)
+            includeNonPublic.set(false)
+            skipEmptyPackages.set(true)
+            reportUndocumented.set(true)
+            suppressObviousFunctions.set(true)
+
+            // Link the source to the documentation
+            sourceLink {
+                localDirectory.set(file("src"))
+                remoteUrl.set(URL("https://github.com/$repo/tree/trunk/src"))
+            }
+
+            // JNA external documentation links
+            externalDocumentationLink {
+                url.set(URL("https://javadoc.io/doc/net.java.dev.jna/jna/5.10.0/"))
             }
         }
     }
@@ -38,12 +71,16 @@ tasks {
     /* Artifacts */
 
     // Source artifact, including everything the 'main' does but not compiled.
-    val sourcesJar by registering(Jar::class) {
+    create("sourcesJar", Jar::class) {
+        group = "build"
+
         archiveClassifier.set("sources")
         from(sourceSets.main.get().allSource)
     }
 
-    val javadocJar by registering(Jar::class) {
+    create("javadocJar", Jar::class) {
+        group = "build"
+
         archiveClassifier.set("javadoc")
         dependsOn(dokkaHtml)
         from(dokkaHtml)
